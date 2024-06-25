36f44c1ecc6137c277582e4f94f9196057d29689
xeisenbeg <bad967975@gmail.com>
2024-06-25 02:48:56 -0700

    fourth commit

---
+++ day12/homework/test.py
@@ -0,0 +1,66 @@
+
+
+# for i in range(10):
+#     print(i)
+
+
+
+
+# name =  "nika"
+# for i in range(5):
+#     print(name)
+
+
+
+
+# num = 1
+# while num < 6:
+#   print(num)
+#   num += 1
+
+
+# age = int(input("enter your age "))
+# if age >= 18 and age < 80:
+#     print('you are over 18')
+# elif age > 80:
+#     print(" please enter correct age")
+# elif age < 18:
+#     print("you are child")
+
+# else:
+#     print("please enter correct age")
+
+
+
+
+
+# number = int(input("enter number that equals 10 - "))
+# if number == 10:
+#     print("Correct")
+# else:
+#     print("invalid")
+
+
+
+
+# number = int(input("enter number - "))
+# if number > 0 and number < 10:
+#     print("child")
+# else:
+#     print("not child")
+
+
+# triangle_corner1 = int(input("enter first corner of Equilateral Triangle - "))
+# triangle_corner2 = int(input("enter second corner of  Equilateral Triangle - "))
+# triangle_corner3 = int(input("enter third corner of Equilateral Triangle - "))
+# triangle = (triangle_corner1 + triangle_corner2 + triangle_corner3)
+# # print("your triangle is",triangle_corner1 + triangle_corner2 + triangle_corner3,"degrees")
+# if triangle == 180:
+#     print("your triangle is correct")
+# elif triangle < 180 or triangle > 180:
+#     print("your triangle is",triangle_corner1 + triangle_corner2 + triangle_corner3,"degrees, not possible to build equilateral triangle")
+# else:
+#     print("enter correct numbers")
+
+
+ 
