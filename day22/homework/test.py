932c02c8792d429928792aab3dedbab542731770
xeisenbeg <bad967975@gmail.com>
2024-11-12 10:07:43 -0800

    commit 22

---
+++ day22/homework/test.py
@@ -0,0 +1,68 @@
+#1
+# fruit_list = ['apple', 'watermelon', 'orange', 'peach', 'grapes']
+# print(fruit_list)
+# print(fruit_list + ['strawberry'])
+
+
+
+
+
+#2
+# movie_list = ['clockwork orange', 'eyes wide shut', 'speak no evil', 'venom', 'midsommar', 'deadpool', 'interstaller','the dark knight','shutter island'] 
+# print(movie_list[0:8])
+
+
+
+#3
+# num_list = [1,2,3,4,5,6,7,8,9,10]
+# for i in range(len(num_list)):
+#     print(num_list[i])
+
+
+#4
+# animal_list = [input('enter animals you love - ')]
+# print(animal_list)
+
+#5
+# names = ['gio', 'nika', 'davit', 'mariam', 'tornike', 'anastasia', 'beqa']
+# for i in range(len(names)):
+#     print('გამარჯობა ' + names[i])
+
+
+
+#6
+# colors = ['red', 'green', 'blue', 'gray']
+# colors.insert(4, "purple")
+# print(colors)
+
+
+#7
+# numbers_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# for num in numbers_list[::-1]:
+#     print(num)
+
+
+#8
+# books_list = ['the hunger games', 'harry potter', 'to kill a mockingbird', 'the book thief']
+# i = books_list.index('harry potter')
+# print(i)
+
+
+#9
+# city = ['dubai', 'New York', 'Manchester', 'tbilisi', 'gori']
+# city.reverse()
+# print(city)
+
+#10
+# fruits = ['apple', 'banana', 'watermelon']
+# print(fruits)
+
+#11
+# nums = [1,2,3,4,5,6,7,8,9]
+# print(nums[1])
+
+
+
+#12
+# city = ['dubai', 'New York', 'Manchester', 'tbilisi', 'gori']
+# print(len(city))
