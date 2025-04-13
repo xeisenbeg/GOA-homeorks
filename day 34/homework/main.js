51459fad4cdbcc2c2e24aed84abf8a4f2c2f1756
xeisenbeg <139885098+xeisenbeg@users.noreply.github.com>
2025-04-13 19:48:48 +0400

    Add files via upload

---
+++ day 34/homework/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
